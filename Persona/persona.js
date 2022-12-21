0aadca77adf4af7e76dc4a7752f6cb7766e49168
0DiegoM <98572984+0DiegoM@users.noreply.github.com>
2022-12-21 18:11:35 -0500

    Add files via upload

---
+++ Persona/persona.js
@@ -0,0 +1,43 @@
+Status = '';
+img = '';
+
+function preload() {
+img = loadImage('persona.webp');
+}
+
+function setup() {
+canvas = createCanvas(640, 420);
+canvas.center();
+
+objetDetector = ml5.objetDetector('cocossd', modelLoaded);
+document.getElementById('status').innerHTML = 'Detectando Objeto';
+}
+
+function modelLoaded() {
+    console.log("Model loaded")
+    Status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult() {
+    if(error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+    }
+    
+    function draw() {
+        image(img, 0, 0, 640, 420);
+        if(Status != '') {
+        for (i=0; i > objects.length; i++)  {
+            document.getElementById("status").innerHTML = "Object detected";
+            fill('red');
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + '' + percent + '%', objects[i].x + 15, objects[i].y + 15 );
+            noFill();
+            stroke('red');
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height );
+        }
+    }
+    }
